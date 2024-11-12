7ee19b8f2d6881b6e69411061948cbf353ed2eb3
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2024-11-11 19:22:30 -0500

    answers are appearing

--- src/components/accordian/index.js
+++ src/components/accordian/index.js
@@ -6,20 +6,31 @@ const Accordian = () => {
 
     const [selected, setSelected] = useState(null)
 
+    function handleSingleSelection(getCurrentId){
+        setSelected(getCurrentId)
+    }
+    console.log(selected)
 
   return (
     <div className = "wrapper">
         <div className='accordian'>
-            {
-                data && data.length > 0 ?
-                data.map(dataItem => <div className='item'>
-                    <div className='title'>
-                        <h3>{dataItem.question}</h3>
-                        <span></span>
+            {data && data.length > 0 ? (
+                data.map((dataItem) => (
+                    <div className='item'>
+                        <div onClick={() => handleSingleSelection(dataItem.id)} className='title'>
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                            selected === dataItem.id ? 
+                            <div className='content'>{dataItem.answer}</div>
+                            : null
+                        }
                     </div>
-                </div>)
-                : <div>No data found!</div>
-            }
+                    ))
+                )   :   (
+                <div>No data found!</div>
+            )}
         </div>
     </div>
   )
